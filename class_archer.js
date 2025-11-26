d8efd238a3c2d6e27eb43c0098684b54065887b6
Lewangolky <Lewangolky@users.noreply.github.com>
2025-11-26 02:51:09 +0000

    deploy: 18eabdcb85e6b8b0a98017eb26342ea5a90226ad

--- class_archer.js
+++ class_archer.js
@@ -1,7 +1,5 @@
 var class_archer =
 [
-    [ "Archer", "class_archer.html#ad58b47949572ab03748feb5ab6240e79", null ],
     [ "attack", "class_archer.html#abf95cd9c186e0abd48b2e5f76e767868", null ],
-    [ "dodge", "class_archer.html#ae6167f24cf0fe176a21321b928f3b5db", null ],
-    [ "agility", "class_archer.html#aed09561780874c44dd9ef8071a6ba9a5", null ]
+    [ "dodge", "class_archer.html#ae6167f24cf0fe176a21321b928f3b5db", null ]
 ];
--- class_skill_tree_node.js
+++ class_skill_tree_node.js
@@ -1,14 +1,5 @@
 var class_skill_tree_node =
 [
-    [ "SkillTreeNode", "class_skill_tree_node.html#a2d8dda3775a56a0b85fbdb1e210a37d4", null ],
-    [ "~SkillTreeNode", "class_skill_tree_node.html#ab2632f1001fc04f580b4c64182c1b9cc", null ],
     [ "addChild", "class_skill_tree_node.html#afe6a9ea3e96da09800ec6da4099c01ae", null ],
-    [ "dfs", "class_skill_tree_node.html#a3b55c509608e4a39b922cf58d51cf082", null ],
-    [ "getChildrenRaw", "class_skill_tree_node.html#af6c2d6a31f4a211be42d08e4b71486e0", null ],
-    [ "getParent", "class_skill_tree_node.html#aa4a97024a905c5f3408af361f78614dd", null ],
-    [ "getSkill", "class_skill_tree_node.html#a5c2ee133f0cdb33e4b0f19be1b4c6288", null ],
-    [ "removeChildWithSkillName", "class_skill_tree_node.html#a23ab889810393ec3d48ef383b1556cdb", null ],
-    [ "children", "class_skill_tree_node.html#a66f4af4724821cb1839fb2990721e476", null ],
-    [ "parent", "class_skill_tree_node.html#af164352884d09d9ef92201af49714236", null ],
-    [ "skill", "class_skill_tree_node.html#aefad2a267691d21ccd8a02a799173f0f", null ]
+    [ "dfs", "class_skill_tree_node.html#a3b55c509608e4a39b922cf58d51cf082", null ]
 ];
